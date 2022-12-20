4e147a3b94585ffe67d88d4043fe04b07ff5ce2d
Kgchek <rptr22f@msn.com>
2022-12-20 14:55:37 -0700

    Add delete button to cart/works

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,22 +1,46 @@
 import React, { useState } from 'react';
 
-
 export default function Cart({ cart, setCart }) {
+  const removeItemFromCart = async (itemToRemove) => {
+    try {
+      await fetch(`/api/cart/${itemToRemove._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('jwtToken')}`,
+        },
+      });
 
- 
-  
-  
-  return ( 
+      // Remove the item from the cart state
+      setCart({
+        ...cart,
+        lineItems: cart.lineItems.filter((item) => item._id !== itemToRemove._id),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const totalCost = cart.lineItems.reduce((acc, item) => acc + item.item.price, 0);
+
+  return (
     <div>
-       {cart.lineItems && cart.lineItems.length === 0 ?
-       'Your cart is empty' : 
-       cart.lineItems && cart.lineItems.map((item) => (
-        <div>
-         {item.item.title}
-         <img src={item.item.main_image} alt="" />
-         <button>Remove</button>
-        </div>
-         
-       ))}
-       </div>
-  )}   
+      {cart.lineItems && cart.lineItems.length === 0 ? (
+        'Your cart is empty'
+      ) : (
+        cart.lineItems &&
+        cart.lineItems.map((item) => (
+          <div>
+            {item.item.title} 
+            &nbsp;
+            <img src={item.item.main_image} alt="" /> 
+            &nbsp;
+            {item.item.price}
+            <button onClick={() => removeItemFromCart(item)}>Remove</button>
+          </div>
+        ))
+      )}
+      <div>Total: {totalCost}</div>
+    </div>
+  );
+}
